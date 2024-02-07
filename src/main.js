c511fc397dffc387982884802e4cf4420bf0f43d
KiD <nasrullahshahdan@gmail.com>
2024-02-07 23:18:55 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles//header.css';
+import '../styles/section.css';
+import '../styles/container.css';
+import '../styles/contact.css';
+import '../styles/popCarta.css';
+import '../styles/utils.css';
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        index: '/index.html',
+        kertasKerja: '/kertasKerja.html',
+        borang: 'borang.html',
+        jadual: 'jadual.html',
+        surat: 'surat.html'
+      }
+    }
+  }
+}
